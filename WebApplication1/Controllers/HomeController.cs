d08b505873948aa3021648c44d0a746277a54232
USER <alishka1417@gmail.com>
2023-01-25 14:03:01 +0500

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ViewResult Index()
+		{
+			int hour = DateTime.Now.Hour;
+			ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Доброго дня";
+			return View();
+		}
+		[HttpGet]
+		public ViewResult RsvpForm()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ViewResult RsvpForm(GuestResponse guest)
+		{
+			if (ModelState.IsValid)
+				return View("Thanks", guest);
+			else
+				return View();
+        }
+	}
+}
